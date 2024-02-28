16a32e17753bf5aacab87b34c6bd1b51637e5f43
elvis <elvisnnadi38@gmail.com>
2024-02-28 17:04:28 +0100

    initail commit fo the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HospitalComponent } from './hospital/hospital.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent,
+    Section2Component,
+    Section3Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
